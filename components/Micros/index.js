1e51e75b9ef94620637babdf64c3366288f265a7
Rahul SriRam <18131a0505@gvpce.ac.in>
2022-01-26 22:45:48 +0530

    Delete components/Micros directory

--- components/Micros/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ActiveLink } from './ActiveLink';
-export { default as DarkModeSwitch } from './DarkModeSwitch';
-export { default as ScrollToTop } from './ScrollToTop';
-export { default as ThemeChooser } from './ThemeChooser';
